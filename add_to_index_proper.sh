136af34a73cfb21f6599e6a8a8812f51222fa3a7
b4rkod <void@Asena.localdomain>
2023-11-21 22:26:56 +0300

    proper.sh added

---
+++ add_to_index_proper.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+FILE=$(basename $1|sed s/.txt/.html/)
+echo $FILE
+/home/void/www/sh/formatter.sh /home/void/www/blogposts/$(basename $1) >| "$HOME/www/source/blogposts/$FILE"
+scp $HOME/www/source/blogposts/$FILE root@23.94.92.217:/var/www/b4rkod/blogposts/ &
+$HOME/www/sh/add_to_rss.sh $1
+sed -i "/\<ul name=blogposts\>/s/$/\n\t\<li\>\<a href=\"blogposts\/$FILE\"\>$FILE\<\/a\>\<\/li\>/" $HOME/www/source/index.html
+scp $HOME/www/source/{index.html,rss.xml} root@23.94.92.217:/var/www/b4rkod/ &
+
--- formatter.sh
+++ formatter.sh
@@ -1,7 +1,7 @@
-printf "<html>\n <head>\n <meta charset="utf-8">\n<link rel="ICON" href="/etc/favicon.ico"/>\n<link rel="stylesheet" href="/etc/style.css">\n <title>Barkod's Basement</title>\n <h1 align="center"> $(basename $1|sed s/txt$/html/) </h1> </head>\n <body>\n<hr>\n "
-printf "<link rel="apple-touch-icon" sizes="180x180" href="/etc/apple-touch-icon.png">\n <link rel="icon" type="image/png" sizes="32x32" href="/etc/favicon-32x32.png">\n <link rel="icon" type="image/png" sizes="16x16" href="/etc/favicon-16x16.png">\n <link rel="manifest" href="/etc/site.webmanifest">\n <link rel="mask-icon" href="/etc/safari-pinned-tab.svg" color="#5bbad5">\n <meta name="msapplication-TileColor" content="#da532c">\n <meta name="theme-color" content="#ffffff">\n"
+printf "<html> <head> <meta charset="utf-8"> <link rel="ICON" href="/etc/favicon.ico"/> <link rel="stylesheet" href="/etc/style.css"> <title>Barkod's Basement</title> <h1 align="center"> $(basename $1|sed s/txt$/html/) </h1> </head> <body><hr> "
+printf "<link rel="icon" type="image/png" sizes="32x32" href="/etc/favicon-32x32.png">\n <link rel="icon" type="image/png" sizes="16x16" href="/etc/favicon-16x16.png">\n <link rel="manifest" href="/etc/site.webmanifest">\n <meta name="theme-color" content="#ffffff">\n"
 printf "<div id="menu" >\n <a href="/index.html" >Main</a>\n <a href="/index.html#blogposts" >Blogposts</a>\n <a href="/index.html#contents" >Contents</a>\n <a href="files/files.html" >Library</a> \n <a href="https://git.b4rkod.net.tr" >git</a>\n <a href="https://b4rkod.net.tr/rss.xml" >RSS</a> <a href="https://b4rkod.net.tr/#webrings" >webrings</a></div>\n "
 
-printf "<p> date : $(stat -c '%w' $1| cut -d " " -f 1)</p>\n "
-sed "s/^/<p> \&nbsp \&nbsp /;s/$/<\/p>/" $1
-printf  " </body>\n <html>\n"
+printf "<p> date : $(stat -c '%w' $1| cut -d " " -f 1)\n "
+sed "/\/\//s/$/<\/p>/;/\/\//s/\/\//<p> \&nbsp \&nbsp /" $1
+printf  " </body> <html>"
