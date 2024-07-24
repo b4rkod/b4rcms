e0e411023758782320154ab663bc5673e3be2a6a
b4rkod <lllillilll@protonmail.com>
2024-07-24 15:47:22 +0300

    deleted unnecesary files

--- add_to_rss.sh
+++
@@ -1,8 +0,0 @@
-FILE=$(cat $1 | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g'| sed "s#\/#\\\/#g" )
-
-BASE=$(basename $1)
-
-RSS_DATE=$(date -d "$(stat -c %w $1)" -R)
-sed -i "/<\!--item-->/s/$/\n<item>\n<pubDate>$RSS_DATE<\/pubDate>\n<guid>https:\/\/b4rkod.net.tr\/$BASE<\/guid>\n<title>$BASE<\/title>\n<link>https:\/\/b4rkod.net.tr\/blogposts\/$BASE<\/link>\n<description><![CDATA[$FILE]]><\/description>\n<\/item>/" "$HOME/www/source/rss.xml"
-
-
--- test.sh
+++
@@ -1,6 +0,0 @@
-. ~/.config/b4rcms/basics.sh
-
-for FILE in $(ls $ROOT/blogposts); do
-	DATE=$(date -d @$(grep $FILE $DB/blogposts.txt |cut -c -10) -R )
-	echo $DATE
-done
